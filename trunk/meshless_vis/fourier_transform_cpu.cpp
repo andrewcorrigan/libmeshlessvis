38fc5e4e7e9ba4e4325a44bc697ce19c4cada569
andrew.corrigan <andrew.corrigan@7b010d5c-0e46-0410-ab28-4f22c37eb7d4>
2008-04-09 14:16:31 +0000

    slight changes to the makefiles and where _LIBMESHLESSVIS_USE_CPU is defined

--- trunk/meshless_vis/fourier_transform_cpu.cpp
+++ trunk/meshless_vis/fourier_transform_cpu.cpp
@@ -19,7 +19,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
 
-#define _LIBMESHLESSVIS_USE_CPU
 #include "fourier_transform_cpu.h"
 #include <cmath>
 #include <iostream>
